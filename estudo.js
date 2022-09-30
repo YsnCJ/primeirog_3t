5a9252dbee55497122ed700a77e6bbc576938c52
YsnCJ <108945302+YsnCJ@users.noreply.github.com>
2022-09-30 12:51:00 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,14 +1,16 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Primeiro G</title>
-    <script src="estudo.js"></script>
+let tabuada = 5;
 
-</head>
-<body>
-    <button onclick="escreva()">CLIQUE AQUI</button>
-</body>
-</html
+function escreva(){
+    document.write("Tabuada do " + "<br>");
+    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
+
+}
