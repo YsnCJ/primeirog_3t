36f96870d83159836096995c9fe02eb706bcb67d
YsnCJ <108945302+YsnCJ@users.noreply.github.com>
2022-10-26 11:54:32 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,15 +1,32 @@
 function total(){
-    let c = document.getElementById("valor").value;
+    let c = document.getElementById("capital").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
-    let r = 0;
-    for(let i = 1; i <= t; i++){
-        r = c * (1 + (j/100));
-        document.write("Mês " + i + " valor: " + r +"<br>");
-        c = r;
+    if(!Number(c)){
+      alert("o valor dos juros deve ser um número.");
+      document.getElementById("capital").value = "";
+      document.getElementById("capital").focus();
+      return
     }
-    
-    document.write("Resultado: " + r);
+    if(!Number(j)){
+      alert("o valor do capital deve ser um número.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
+      return
+    }
+    if(!Number(t)){
+      alert("o valor dos meses deve ser um número.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus();
+      return
+    }
+    let m = 0;
+    for(let i = 1; i <= t; i++){
+        m = c * (1 + (j/100));
+        document.write("Valor no mês " + i + " = " + m + "<br>");
+        c = m;
+    }
+    document.write("Montante: " + m);
 }
 function adicao(){
     let val1 = document.getElementById("v1").value;
@@ -46,5 +63,3 @@ function adicao(){
             let r = Number(val1) * Number(p);
             document.getElementById("resultado").innerHTML =r;
             } 
-
-
